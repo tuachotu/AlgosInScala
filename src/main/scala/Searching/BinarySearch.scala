bd14eb42ddb01997c1a5b79b1990aa16350e2007
vikrant <vikrant.thakur@gmail.com>
2022-11-18 09:51:05 -0800

    fixing bug in binary search

--- src/main/scala/Searching/BinarySearch.scala
+++ src/main/scala/Searching/BinarySearch.scala
@@ -4,17 +4,21 @@ object BinarySearch extends App {
   //
   def binarySearch(list: List[Int], value: Int): Option[Int] = {
     def binarySearchInternal(start: Int, end: Int): Option[Int] = {
-      if (start > end)  None
-      (start + end)/2 match {
-        case m if value < list(m) => binarySearchInternal(start, m-1)
-        case m if value > list(m) => binarySearchInternal(m+1, end)
-        case m  => Some(m)
+      if (start >= end) {  None } else {
+        (start + end) / 2 match {
+          case m if value < list(m) => binarySearchInternal(start, m - 1)
+          case m if value > list(m) => binarySearchInternal(m + 1, end)
+          case m => Some(m)
+        }
       }
     }
     binarySearchInternal(0, list.length - 1 )
     }
     
-    println(binarySearch(List(1,2,3,4,5,6,7,8), 3))
+    //println(binarySearch(List(1,2,3,4,5,6,7,8), 3))
+    println(binarySearch(List(1,2,3,4,5,6,7,8), 12))
+    println(binarySearch(List(1,2,3,4,5,6,7,8), 0))
+    println(binarySearch(List(1,2,3,4,5,6,7,8), 7))
   }
 
 
