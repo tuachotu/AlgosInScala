38a1e8f5e96c61dcfc3edd19310e9d755c4a1f68
tuachout <vikrant.thakur@gmail.com>
2022-11-29 11:02:31 -0800

    Added new implementation for Kth largest element

--- src/main/scala/Searching/KthLargestElement.scala
+++ src/main/scala/Searching/KthLargestElement.scala
@@ -1,6 +1,31 @@
 package Searching
 
+import scala.collection.mutable.PriorityQueue
+
 object KthLargestElement extends  App {
+  collection.mutable.PriorityQueue(7, 5, 2, 3, 1)(Ordering[Int].reverse).dequeueAll
+
+  def findKthElementHeap(l: List[Int], k: Int): Option[Int] = {
+    if (l.isEmpty) None
+    else {
+      var myMaxHeap = PriorityQueue[Int]()
+      l foreach( myMaxHeap.enqueue(_))
+      val result = (0 until k).foldLeft(0) { (result, index) => myMaxHeap.dequeue()}
+      Some(result)
+
+    }
+  }
+
+  def findKthSmallestElementHeap(l: List[Int], k: Int): Option[Int] = {
+    if (l.isEmpty) None
+    else {
+      var myMaxHeap = PriorityQueue[Int]()(Ordering[Int].reverse)
+      l foreach (myMaxHeap.enqueue(_))
+      val result = (0 until k).foldLeft(0) { (_, _) => myMaxHeap.dequeue() }
+      Some(result)
+
+    }
+  }
 
   def findKthElement(l: List[Int], k: Int): Option[Int] = {
     if (l.isEmpty ) None //TODO: Check for boundary
@@ -15,6 +40,9 @@ object KthLargestElement extends  App {
     }
   }
 
-  println(findKthElement(List(9,8,7,6,5,4,3,2,1), 9-6))
+  println(findKthElement(List(9,8,7,6,5,4,3,2,1), 9-3+ 1))
+  println(findKthElement(List(9,8,7,6,5,4,3,2,1), 3))
+  println(findKthElementHeap(List(9,8,7,6,5,4,3,2,1), 3))
+  println(findKthSmallestElementHeap(List(9,8,7,6,5,4,3,2,1), 3))
 
 }
