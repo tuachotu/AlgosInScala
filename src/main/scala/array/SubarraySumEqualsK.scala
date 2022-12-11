5b562358aa2eaa398acae7bc27391b8f382824de
tuachout <vikrant.thakur@gmail.com>
2022-12-10 22:25:08 -0800

    more code

---
+++ src/main/scala/array/SubarraySumEqualsK.scala
@@ -0,0 +1,49 @@
+package array
+
+
+/*
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ */
+
+object SubarraySumEqualsK extends App{
+  def findArrayCountBruteForce(nums:List[Int], target: Int): Int = {  // O(n3)
+    val sums = for {
+      i <- nums.indices
+      j <- i until nums.length
+    } yield  /*nums.drop(i).take(j-i+1)*/ nums.slice(i,j+1).sum
+
+    sums.count(_ == target)
+  }
+
+  // populate an array with sum till  each index
+  // if difference of sum at each index is same as target then that index pair is result
+  def findArrayCountWithSumWithSpace(nums:List[Int], target: Int): Int = {
+    val lookup = nums.foldLeft(List[Int](0)) { (lookupInProgress, number) =>
+       lookupInProgress ++ List(lookupInProgress.last + number)
+    }
+    val res =  for {
+      i <- nums.indices
+      j <- i+1 to nums.length // tricky part
+    } yield lookup(j) - lookup(i)
+    res.count(_ == target)
+  }
+
+  println(findArrayCountBruteForce(List(1,1,1), 2))
+  println(findArrayCountWithSumWithSpace(List(1,1,1), 2))
+  println
+  println(findArrayCountBruteForce(List(1,2,3), 3))
+  println(findArrayCountWithSumWithSpace(List(1, 2,3), 3))
+  println
+  println(findArrayCountBruteForce(List(1,2,3,15), 15))
+  println(findArrayCountWithSumWithSpace(List(1,2,3,15), 15))
+  println
+  println(findArrayCountBruteForce(List(1,-1, 2,3,15, -12,-3), 0))
+  println(findArrayCountWithSumWithSpace(List(1,-1, 2,3,15, -12,-3), 0))
+
+}
